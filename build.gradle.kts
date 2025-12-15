c7b7539cbee6c51fbcc55e7d67103850b6de7294
eddgrant <edd@eddgrant.com>
2025-12-15 16:14:42 +0000

    **Update Kotest dependencies to version 5.9.0** ```kotlin dependencies {     testImplementation(platform("io.kotest:kotest-bom:5.9.0"))     testImplementation("io.kotest:kotest-runner-junit5")     testImplementation("io.kotest:kotest-assertions-core") } ```

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
-    testImplementation("io.kotest:kotest-assertions-core:5.8.0")
+    testImplementation(platform("io.kotest:kotest-bom:5.9.0"))
+    testImplementation("io.kotest:kotest-runner-junit5")
+    testImplementation("io.kotest:kotest-assertions-core")
 }
 
 tasks.test {
